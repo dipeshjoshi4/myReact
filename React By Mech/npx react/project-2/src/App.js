c056d8675a3cfb997caede4256fd293099fe0eb2
dipeshjoshi <dipeshj2310@gmail.com>
2024-03-14 12:57:07 +0530

    added

--- React By Mech/npx react/project-2/src/App.js
+++ React By Mech/npx react/project-2/src/App.js
@@ -3,15 +3,12 @@ import "./App.css"
 
 //Function component
 
-const name = "var";
+
 
 function App() {
     return (
        <div className="app">
            <h1>Hello From App Component</h1>
-            <p>dipesh</p>
-            <h2>{ name}</h2>
-            <p>{5+5}</p>
        </div>
         )     
 }
--- React By Mech/npx react/project-2/src/index.js
+++ React By Mech/npx react/project-2/src/index.js
@@ -6,5 +6,5 @@ import App from "./App"
 
 ReactDOM.render(
     <>
-        <App/>
+    <App/>
     </> ,document.getElementById("root"));
